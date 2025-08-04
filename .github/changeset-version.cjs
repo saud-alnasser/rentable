aa7cbc78bad20ddf07ef356605e9912dc3d810f3
SAUD ALNASSER <saud4services@gmail.com>
2025-08-04 13:45:38 +0300

    initial commit

---
+++ .github/changeset-version.cjs
@@ -0,0 +1,12 @@
+// credits https://github.com/t3-oss/create-t3-app/blob/next/.github/changeset-version.js
+
+const { execSync } = require('node:child_process');
+
+// this script is used by the `release.yaml` workflow to update the version of the packages being released.
+// the standard step is only to run `changeset version` but this does not update the `package-lock.json` or `cargo.lock` file.
+// so we also run `pnpm install --lockfile-only`, which does this update also we run `cargo update`.
+// this is a workaround until this is handled automatically by `changeset version`.
+// see https://github.com/changesets/changesets/issues/421.
+execSync('pnpm changeset version');
+execSync('pnpm install --lockfile-only');
+execSync('cd system && cargo update');
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/core';
+	import { onMount } from 'svelte';
+	import '../app.css';
+
+	onMount(() => invoke('show_window'));
+
+	let { children } = $props();
+</script>
+
+{@render children()}
